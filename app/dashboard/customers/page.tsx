babc1d0b20b858df20193d7306ff03713ac3cd28
kyohei.hara <kuru0203.hara@gmail.com>
2025-08-06 16:43:03 +0900

    add customers

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,27 @@
+import Table from "@/app/ui/customers/table";
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+import { Suspense } from "react";
+import { fetchFilteredCustomers } from "@/app/lib/data";
+
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Customers",
+  title: "Custmers",
 };
 
-export default function Page() {
-  return <p>customers Page</p>;
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || "";
+  const customers = await fetchFilteredCustomers(query);
+  return (
+    <div className="w-full">
+      <Suspense fallback={<InvoicesTableSkeleton />}>
+        <Table customers={customers} />
+      </Suspense>
+    </div>
+  );
 }
